802949ae0241a56c31c37658b3c0cf67a1fdcadb
pixelblob <phillipsfamily.ethan@gmail.com>
2024-02-25 01:51:00 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         TikTok Desktop Ad Remover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Removes tiktok ads through fetch intercept
+// @author       PixelBlob
+// @match        https://www.tiktok.com/*
+// @grant       GM_xmlhttpRequest
+// @license MIT
+// @run-at      document-start
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tiktok.com
+// ==/UserScript==
+ 
+(function() {
+    'use strict';
+ 
+    unsafeWindow.originalFetch = unsafeWindow.fetch
+    unsafeWindow.fetch = async (...args) => {
+        let response = await unsafeWindow.originalFetch(...args);
+ 
+        if (response.url.includes("item_list")) {
+            const json = () =>
+            response
+            .clone()
+            .json()
+            .then((data) => {
+                var adCount = data.itemList.filter(item=>item.ad_info).length
+                if (adCount > 0) console.log(`Removed ${adCount} Ads!`)
+                data.itemList = data.itemList.filter(item=>!item.ad_info)
+                return ({ ...data })
+            });
+ 
+            response.json = json;
+ 
+ 
+        }
+ 
+        return response;
+    };
+})();
